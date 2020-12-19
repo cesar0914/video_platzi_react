68e149b28d29eedf7fe06fa09bfe60ea909cc437
Cesar <cesarvasquez0914@gmail.com>
2020-12-19 01:49:51 -0500

    Commit final proyecto terminado

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
-import '../assets/styles/App.scss';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../Hooks/useInitialState';
+
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState';
+
+
+
 
 const App = () => {
+    
+    const initalState =  useInitialState(API);   
     return (
         <div className = 'App'>
              <Header />
              <Search />
+            {
+                // si el arreglo miLista[] no tiene información no se muestra en pantalla
+                initalState.mylist.length > 0 && 
+                <Categories title='Mi lista'>
+                    <Carousel>
+                    {
+                        initalState.mylist.map(item =>
+                            <CarouselItem key={item.id} {...item} />      
+                        )                       
+                    }                                   
+                    </Carousel>
+                </Categories>  
+            }
+            
+            <Categories title='Tendencias'>
+                <Carousel>
+                    {
+                        initalState.trends.map(item =>
+                            <CarouselItem key={item.id} {...item} />      
+                        )                       
+                    }                                                      
+                </Carousel>
+            </Categories>  
 
-             <Categories>
-                 <Carousel>
-                     <CarouselItem />
-                     <CarouselItem />
-                     <CarouselItem />
-                     <CarouselItem />
-                     <CarouselItem />
-                 </Carousel>
-             </Categories>
+            <Categories title='Originales de platzi video'>
+                <Carousel>
+                    {
+                        initalState.originals.map(item =>
+                            <CarouselItem key={item.id} {...item} />      
+                        )   
+                    }                                 
+                </Carousel>
+            </Categories>  
+             
+             <Footer />
         </div>
     );
 }
